9259b76129db42dff649b6fb199259da5548711b
Akrecy23 <167392585+Akrecy23@users.noreply.github.com>
2025-12-31 00:24:16 +0800

    Update ExpensesDetails_Card.js

--- expenses_script/ExpensesDetails_Card.js
+++ expenses_script/ExpensesDetails_Card.js
@@ -57,7 +57,7 @@ async function fetchTripsAndRenderTabs(yearId, countryFilters, groupFilters) {
     .get();
 
   const collabSnap = await window.db.collection("Trips")
-    .where(`collaborators.${currentUserId}`, "!=", null)   // field exists
+    .where("collaboratorIds", "array-contains", currentUserId)
     .where("year", "==", parseInt(yearId))
     .get();
 
@@ -204,6 +204,7 @@ function parseDate(dateStr) {
   return new Date(cleaned);
 
 }
+
 
 
 
