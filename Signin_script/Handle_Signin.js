179cb34949373c5e41e078a109c35bee67774003
Akrecy23 <167392585+Akrecy23@users.noreply.github.com>
2026-01-05 15:48:02 +0800

    Update Handle_Signin.js

--- Signin_script/Handle_Signin.js
+++ Signin_script/Handle_Signin.js
@@ -18,8 +18,8 @@ async function handleGoogleSignIn() {
     if (result.additionalUserInfo.isNewUser) {
       alert("No account found. Please sign up first.");
       // Delete the just-created Auth user
-      //await user.delete();   // removes the UID from Firebase Auth
-      await window.auth.signOut()
+      await user.delete();   // removes the UID from Firebase Auth
+      // await window.auth.signOut()
       return;
     }
     // Get unique UID
@@ -48,6 +48,7 @@ async function handleGoogleSignIn() {
   }
 
 }
+
 
 
 
