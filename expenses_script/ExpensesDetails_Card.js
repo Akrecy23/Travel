e2735ca9a66d0a3cdec2a9fb997c36e7e8d424f1
Akrecy23 <167392585+Akrecy23@users.noreply.github.com>
2026-01-08 15:46:37 +0800

    Update ExpensesDetails_Card.js

--- expenses_script/ExpensesDetails_Card.js
+++ expenses_script/ExpensesDetails_Card.js
@@ -83,7 +83,7 @@ async function fetchTripsAndRenderTabs(yearId, countryFilters, groupFilters) {
     card.innerHTML = `
       <div class="trip-header">
         <h3 class="trip-id">${tripData.title}</h3>
-        <span class="trip-status">${status}</span>
+        <span class="trip-status ${status.toLowerCase()}">${status}</span>
       </div>
 
       <!-- Dates & Duration -->
@@ -197,6 +197,7 @@ function parseDate(dateStr) {
   return new Date(cleaned);
 
 }
+
 
 
 
